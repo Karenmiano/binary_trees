c8d2a25c780984cbaf0bc44c050728ea43b118c2
Karen Miano <mianokaren1798@gmail.com>
2023-11-01 00:09:10 -0700

    deletes a binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,14 +6,10 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *left, *right;
-
 	if (tree == NULL)
 		return;
 
-	left = tree->left;
-	right = tree->right;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
-	binary_tree_delete(left);
-	binary_tree_delete(right);
 }
