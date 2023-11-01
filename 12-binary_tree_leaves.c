b94441c25bfabbc7d6d195e209509c21aad5a911
Karen Miano <mianokaren1798@gmail.com>
2023-11-01 07:39:41 -0700

    gets number of leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - gets number of leaves in a binary tree
+ * @tree: root node of binary tree
+ * Return: number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->parent && !tree->left && !tree->right)
+		leaves += 1;
+
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+	return (leaves);
+}
